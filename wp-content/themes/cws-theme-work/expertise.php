78f689f40acfeb8bdb0838561960ea4fd31a8905
Blake Baxendell <bbaxendell@consultwebs-email.com>
2017-11-10 10:30:38 -0500

    End of day push

--- wp-content/themes/cws-theme-work/expertise.php
+++ wp-content/themes/cws-theme-work/expertise.php
@@ -4,7 +4,7 @@
  */
 get_header() ?>
 
-<section id="biovu">
+<section id="offerings">
 
 	<div class="left-pill"></div>
 
@@ -18,7 +18,7 @@ get_header() ?>
 
 				<header>
 
-					<h2><?php the_field('offering_title') ?></h2>
+					<h2>Our Offerings</h2>
 
 				</header>
 
@@ -38,17 +38,11 @@ get_header() ?>
 
 			<div class="col-md-offset-1 col-md-5">
 
-				<div class="pr-40">
+				<div class="border-box">
 
-					<div class="border-box">
+					<i class="compass-icon"></i>
 
-						<i class="compass-icon"></i>
-
-						<h3><!--title--></h3>
-
-						<!--box content-->
-
-					</div>
+					<?php the_field('left_box_content') ?>
 
 				</div>
 
@@ -56,17 +50,11 @@ get_header() ?>
 
 			<div class="col-md-offset-1 col-md-5">
 
-				<div class="pr-40">
+				<div class="border-box">
 
-					<div class="border-box">
+					<i class="pill-icon"></i>
 
-						<i class="compass-icon"></i>
-
-						<h3><!--title--></h3>
-
-						<!--box content-->
-
-					</div>
+					<?php the_field('right_box_content') ?>
 
 				</div>
 
@@ -74,13 +62,53 @@ get_header() ?>
 
 		</div>
 
-		<div class="row">
+		<div id="projects" class="row">
 
-			<div id="projects" class="col-md-6 center-col text-center">
+			<header class="col-md-6 center-col text-center no-pad">
 
 				<h2>Example Projects</h2>
 
-				<!--projects content-->
+				<?php the_field('projects_content') ?>
+
+			</header>
+
+			<div class="clear"></div>
+
+			<div class="col-md-10 center-col">
+
+				<!-- Nav tabs -->
+				<ul class="nav nav-tabs" role="tablist">
+					<li role="presentation" class="active">
+
+						<!--image-->
+
+						<h3 class="text-uppercase">Early Stage Target Prioritization</h3>
+
+						<a href="#target" aria-controls="target" role="tab" data-toggle="tab">View Case Study</a>
+
+					</li>
+
+					<li role="presentation">
+
+						<!--image-->
+
+						<h3 class="text-uppercase">Target Disease Deep Dives</h3>
+
+						<a href="#deep-dive" aria-controls="deep-dive" role="tab" data-toggle="tab">View Case Study</a>
+
+					</li>
+
+					<li role="presentation">
+
+						<!--image-->
+
+						<h3 class="text-uppercase">Cohort Analysis and Screening</h3>
+
+						<a href="#cohort" aria-controls="cohort" role="tab" data-toggle="tab">View Case Study</a>
+
+					</li>
+
+				</ul>
 
 			</div>
 
@@ -90,23 +118,155 @@ get_header() ?>
 
 </section>
 
-<section id="phewas" class="gray-bg">
+<section id="project-tabs" class="lt-blue-bg">
 
 	<div class="container">
 
 		<div class="row">
 
-			<div class="col-md-9 col-md-offset-1">
+			<div class="col-md-10 col-md-offset-1">
 
-				<header>
+				<div class="tab-content">
 
-					<span class="subtitle"><?php the_field('phewas_subtitle') ?></span>
+					<div role="tabpanel" class="tab-pane active" id="target">
+						
+						<header>
 
-					<h2><?php the_field('phewas_title') ?></h2>
+							<h2>Early Stage Target Priotization</h2>
 
-				</header>
+						</header>
 
-				<?php the_field('phewas_content') ?>
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_1') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_2') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_3') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+					</div>
+
+					<div role="tabpanel" class="tab-pane" id="deep-dive">
+						
+						<header>
+
+							<h2>Target Disease Deep Dives</h2>
+
+						</header>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_1') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_2') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_3') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+					</div>
+
+					<div role="tabpanel" class="tab-pane" id="cohort">
+						
+						<header>
+
+							<h2>Cohort Analysis and Screening</h2>
+
+						</header>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_1') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_2') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+						<div class="project-content-block">
+
+							<div class="project-content">
+
+								<?php the_field('early_target_row_3') ?>
+
+							</div>
+
+							<!--image-->
+
+						</div>
+
+					</div>
+
+				</div>
 
 			</div>
 
